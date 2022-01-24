5ce09e73dcd884feea102ac4abe0993139a68890
Pedro Arturo Farias Santillan <pedro.farias.sosa@gmail.com>
2022-01-24 01:19:21 -0600

    Add style background responsive

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,44 @@ export default {
 * {
   box-sizing: border-box;
 }
+:root {
+  --checkbox-color: rgb(202, 60, 60);
+  --checkbox-shadow: rgba(238, 156, 167, 0.2);
+  --add-button: rgba(255, 255, 255, 0.7);
+  --add-button-shadow: rgba(238, 156, 167, 0.4);
+}
 body {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 15px;
   display: flex;
   align-items: center;
   justify-content: center;
+  background-image: linear-gradient(
+      62deg,
+      rgba(7, 86, 160, 0.973) 13%,
+      rgba(255, 122, 151, 0.5) 4%
+    ),
+    linear-gradient(
+      44deg,
+      rgba(0, 43, 99, 0.079) 39%,
+      rgba(242, 140, 143, 0.5) 18%
+    ),
+    linear-gradient(
+      118deg,
+      rgba(84, 202, 242, 0.031) 40%,
+      rgba(247, 155, 187, 0.5) 54%
+    ),
+    linear-gradient(
+      58deg,
+      rgba(90, 90, 237, 0.161) 83%,
+      rgba(249, 156, 142, 0.5) 23%
+    );
+  background-blend-mode: normal, lighten, multiply, hard-light;
+  font-family: "DM Sans", sans-serif;
+  overflow: hidden;
 }
-.container {
+.app-container {
 }
 </style>
